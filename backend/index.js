1113a33cf51e00ec16950b62964799db5d6c5328
rhey_yu <rhey2476@gmail.com>
2024-02-29 21:46:29 -0800

    update

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,11 @@ app.use('/buckets', bucketRoute)
 app.use('/users', userRoute)
 
 // cors
-// app.use(cors());
-app.use(cors({
-  origin: "https://dataset-fullstack.vercel.app/"
-})
-);
+app.use(cors());
+// app.use(cors({
+//   origin: "https://dataset-fullstack.vercel.app/"
+// })
+// );
 
 app.get('/',(req, res) => {
   res.json({msg: 'Server is active, all users account...'})
